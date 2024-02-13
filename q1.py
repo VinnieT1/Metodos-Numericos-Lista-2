4ddf90c5cac6b14f1b674f5d7008052de9af1860
Vinícius Teixeira <92376175+VinnieT1@users.noreply.github.com>
2024-02-13 17:32:55 -0300

    Implemented PtLU decomposition

--- q1.py
+++ q1.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-# resolve Ax = b, onde A é uma matriz quadrada e b é um vetor; x é o vetor solução.
 def eliminacao_gaussiana(A, b):
     m = len(A)
     for i in range(m):
@@ -9,7 +8,7 @@ def eliminacao_gaussiana(A, b):
             if abs(A[j][i]) > abs(A[pivot][i]):
                 pivot = j
 
-        if abs(A[i][i]) < 1e-10 and i == m - 1 and b[pivot] != 0:
+        if abs(A[pivot][i]) < 1e-10 and i == m - 1 and b[pivot] != 0:
             print('Sem solução')
             return None
     
@@ -32,41 +31,57 @@ def eliminacao_gaussiana(A, b):
     return x
 
 def decomposicao_PtLU(A):
+    U = A.copy()
     m = len(A)
-    U = np.copy(A)
-    P = np.eye(m)
     L = np.eye(m)
+    P = np.eye(m)
     
     for k in range(m - 1):
-        i = np.argmax(np.abs(U[k:, k]))
-        U[k, k:m], U[i, k:m] = U[i, k:m].copy(), U[k, k:m].copy()
-        L[k, 1:k - 1], L[i, 1:k - 1] = L[i, 1:k - 1].copy(), L[k, 1:k - 1].copy()
-        P[k,:], P[i,:] = P[i,:].copy(), P[k,:].copy()
+        pivot = np.argmax(np.abs(U[k:, k])) + k
+        if pivot != k:
+            U[[k, pivot], k:] = U[[pivot, k], k:]
+            L[[k, pivot], :k] = L[[pivot, k], :k]
+            P[[k, pivot], :] = P[[pivot, k], :]
         
-        for j in range(k + 1, m):
-            L[j, k] = U[j, k] / U[k, k]
-            U[j, k:m] = U[j, k:m] - L[j, k] * U[k, k:m]
+        for i in range(k + 1, m):
+            fator = U[i, k] / U[k, k]
+            L[i, k] = fator
+            U[i, k:] -= fator * U[k, k:]
     
     return P, L, U
 
-# A = np.array([[2.0, -1.0, 0.0], [-1.0, 2.0, -1.0], [0.0, -1.0, 2.0]])
-# P, L, U = decomposicao_PtLU(A)
-# print(P)
+# ----------------------------------- ELIMINAÇÃO GAUSSIANA -----------------------------------
 
 A1 = np.array([[3.0, -1.0, 0.0], [-1.0, 0.0, -1.0], [0.0, -1.0, 2.0]])
 b1 = np.array([1.0, 0.0, 1.0])
-
 print(eliminacao_gaussiana(A1, b1))
 
 A2 = np.array([[2.0, -1.0, 0.0], [-1.0, 2.0, -1.0], [0.0, -1.0, 2.0]])
 b2 = np.array([1.0, 0.0, 2.0])
-
 print(eliminacao_gaussiana(A2, b2))
 
 A3 = np.array([[6.0, -5.0, 0.0], [15.0, -20.0, 10.0], [0.0, 10.0, -20.0]])
 b3 = np.array([1.0, -1.0, 4.0])
-
-#6x - 5y = 1
-#15x - 20y + 10z = -1
-#10y - 20z = 4
 print(eliminacao_gaussiana(A3, b3))
+
+# ----------------------------------- DECOMPOSIÇÃO PtLU -----------------------------------
+
+P1, L1, U1 = decomposicao_PtLU(A1)
+P1b1 = np.matmul(P1, b1)
+Y1 = eliminacao_gaussiana(L1, P1b1)
+print('P1:', P1)
+print('sol 1:', eliminacao_gaussiana(U1, Y1))
+
+P2, L2, U2 = decomposicao_PtLU(A2)
+P2b2 = np.matmul(P2, b2)
+Y2 = eliminacao_gaussiana(L2, P2b2)
+print('P2:', P2)
+print('sol 2:', eliminacao_gaussiana(U2, Y2))
+
+P3, L3, U3 = decomposicao_PtLU(A3)
+P3b3 = np.matmul(P3, b3)
+Y3 = eliminacao_gaussiana(L3, P3b3)
+print('P3:', P3)
+print('sol 3:', eliminacao_gaussiana(U3, Y3))
+
+# ----------------------------------- CHOLESKY -----------------------------------
