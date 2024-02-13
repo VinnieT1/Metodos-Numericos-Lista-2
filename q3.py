4a8c48ecd031adc215fc39a7303f464de12324fb
Vinícius Teixeira <92376175+VinnieT1@users.noreply.github.com>
2024-02-13 18:09:30 -0300

    q3 finished

--- q3.py
+++ q3.py
@@ -6,8 +6,6 @@ def positiva_definida(n):
     A = A + n * np.eye(n)
     print(A)
     
-# positiva_definida(2)
-# positiva_definida(3)
-# positiva_definida(4)
-
-print(0.5*(np.array([[-2, 0], [0, -2]]) + np.array([[-2, 0], [0, -2]])) + 2*np.eye(2))
+positiva_definida(2)
+positiva_definida(3)
+positiva_definida(4)
