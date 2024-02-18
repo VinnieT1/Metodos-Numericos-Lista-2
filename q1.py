1f48ac148f22f693cc87b94c1dbe6f334ef74124
Vinícius Teixeira <92376175+VinnieT1@users.noreply.github.com>
2024-02-18 15:00:05 -0300

    Ended q5, corrected q1 and q5

--- q1.py
+++ q1.py
@@ -10,6 +10,7 @@ def substituicao_inversa(A, b):
 
 def eliminacao_gaussiana(A, b):
     m = len(A)
+
     for i in range(m):
         pivot = i
         for j in range(i + 1, m):
@@ -54,38 +55,98 @@ def decomposicao_PtLU(A):
     
     return P, L, U
 
+def cholesky_decomposition(A):
+    n = len(A)
+    L = np.zeros((n, n))
+    L[0][0] = np.sqrt(A[0][0])
+
+    for j in range(1, n):
+        L[j][0] = A[j][0] / L[0][0]
+    for i in range(1, n - 1):
+        L[i][i] = np.sqrt(A[i][i] - np.sum(L[i][:i]**2))
+        for j in range(i + 1, n):
+            L[j][i] = (A[j][i] - np.sum(L[j][:i] * L[i][:i])) / L[i][i]
+    L[n - 1][n - 1] = np.sqrt(A[n - 1][n - 1] - np.sum(L[n - 1][:n - 1]**2))
+
+    return L, L.T
+
+def substituicao_direta(A, b):
+    m = len(A)
+    x = np.zeros(m)
+    for i in range(m):
+        x[i] = (b[i] - np.dot(A[i][:i], x[:i])) / A[i][i]
+
+    return x
+
 # ----------------------------------- ELIMINAÇÃO GAUSSIANA -----------------------------------
 
-A1 = np.array([[3.0, -1.0, 0.0], [-1.0, 0.0, -1.0], [0.0, -1.0, 2.0]])
+print('\nELIMINAÇÃO GAUSSIANA\n')
+
+A1 = np.array([[4.0, -1.0, 1.0], [-1.0, 4.0, -1.0], [1.0, -1.0, 4.0]])
 b1 = np.array([1.0, 0.0, 1.0])
-print(eliminacao_gaussiana(A1, b1))
+print('sol 1:', eliminacao_gaussiana(A1, b1))
 
-A2 = np.array([[2.0, -1.0, 0.0], [-1.0, 2.0, -1.0], [0.0, -1.0, 2.0]])
-b2 = np.array([1.0, 0.0, 2.0])
-print(eliminacao_gaussiana(A2, b2))
+A2 = np.array([[0.0, -1.0, 2.0], [-1.0, 2.0, -1.0], [2.0, -1.0, 0.0]])
+b2 = np.array([2.0, 0.0, 1.0])
+print('sol 2:', eliminacao_gaussiana(A2, b2))
 
-A3 = np.array([[6.0, -5.0, 0.0], [15.0, -20.0, 10.0], [0.0, 10.0, -20.0]])
-b3 = np.array([1.0, -1.0, 4.0])
-print(eliminacao_gaussiana(A3, b3))
+A3 = np.array([[2.0, -0.5, 0.0], [-0.5, 2.0, -1.0], [0.0, -1.0, 2.0]])
+b3 = np.array([1.0, -1.0, 7.0])
+print('sol 3:', eliminacao_gaussiana(A3, b3))
 
 # ----------------------------------- DECOMPOSIÇÃO PtLU -----------------------------------
 
+print('\nDECOMPOSIÇÃO PtLU\n')
+
+A1 = np.array([[4.0, -1.0, 1.0], [-1.0, 4.0, -1.0], [1.0, -1.0, 4.0]])
+b1 = np.array([1.0, 0.0, 1.0])
 P1, L1, U1 = decomposicao_PtLU(A1)
 P1b1 = np.dot(P1, b1)
-Y1 = substituicao_inversa(L1, P1b1)
+Y1 = substituicao_direta(L1, P1b1)
+X1 = substituicao_inversa(U1, Y1)
 print('P1:', P1)
-print('sol 1:', substituicao_inversa(U1, Y1))
+print('sol 1:', X1)
 
+A2 = np.array([[0.0, -1.0, 2.0], [-1.0, 2.0, -1.0], [2.0, -1.0, 0.0]])
+b2 = np.array([2.0, 0.0, 1.0])
 P2, L2, U2 = decomposicao_PtLU(A2)
-P2b2 = np.matmul(P2, b2)
-Y2 = substituicao_inversa(L2, P2b2)
+P2b2 = np.dot(P2, b2)
+Y2 = substituicao_direta(L2, P2b2)
+X2 = substituicao_inversa(U2, Y2)
 print('P2:', P2)
-print('sol 2:', substituicao_inversa(U2, Y2))
+print('sol 2:', X2)
 
+A3 = np.array([[2.0, -0.5, 0.0], [-0.5, 2.0, -1.0], [0.0, -1.0, 2.0]])
+b3 = np.array([1.0, -1.0, 7.0])
 P3, L3, U3 = decomposicao_PtLU(A3)
-P3b3 = np.matmul(P3, b3)
-Y3 = substituicao_inversa(L3, P3b3)
+P3b3 = np.dot(P3, b3)
+Y3 = substituicao_direta(L3, P3b3)
+X3 = substituicao_inversa(U3, Y3)
 print('P3:', P3)
-print('sol 3:', substituicao_inversa(U3, Y3))
+print('sol 3:', X3)
 
 # ----------------------------------- CHOLESKY -----------------------------------
+
+print('\nCHOLESKY\n')
+
+A1 = np.array([[4.0, -1.0, 1.0], [-1.0, 4.0, -1.0], [1.0, -1.0, 4.0]])
+b1 = np.array([1.0, 0.0, 1.0])
+L1, L1t = cholesky_decomposition(A1)
+Y1 = substituicao_direta(L1, b1)
+X1 = substituicao_inversa(L1t, Y1)
+print('sol 1:', X1)
+
+A2 = np.array([[2.0, -1.0, 0.0], [-1.0, 2.0, -1.0], [0.0, -1.0, 2.0]])
+b2 = np.array([1.0, 0.0, 2.0])
+L2, L2t = cholesky_decomposition(A2)
+Y2 = substituicao_direta(L2, b2)
+X2 = substituicao_inversa(L2t, Y2)
+print('sol 2:', X2)
+
+
+A3 = np.array([[2.0, -0.5, 0.0], [-0.5, 2.0, -1.0], [0.0, -1.0, 2.0]])
+b3 = np.array([1.0, -1.0, 7.0])
+L3, L3t = cholesky_decomposition(A3)
+Y3 = substituicao_direta(L3, b3)
+X3 = substituicao_inversa(L3t, Y3)
+print('sol 3:', X3)
--- q5.py
+++ q5.py
@@ -62,16 +62,43 @@ def positiva_definida(n):
     A = A + n * np.eye(n)
     return A
 
+def cholesky_decomposition(A):
+    n = len(A)
+    L = np.zeros((n, n))
+    L[0][0] = np.sqrt(A[0][0])
+
+    for j in range(1, n):
+        L[j][0] = A[j][0] / L[0][0]
+    for i in range(1, n - 1):
+        L[i][i] = np.sqrt(A[i][i] - np.sum(L[i][:i]**2))
+        for j in range(i + 1, n):
+            L[j][i] = (A[j][i] - np.sum(L[j][:i] * L[i][:i])) / L[i][i]
+    L[n - 1][n - 1] = np.sqrt(A[n - 1][n - 1] - np.sum(L[n - 1][:n - 1]**2))
+
+    return L, L.T
+
+def substituicao_direta(A, b):
+    m = len(A)
+    x = np.zeros(m)
+    for i in range(m):
+        x[i] = (b[i] - np.dot(A[i][:i], x[:i])) / A[i][i]
+
+    return x
+
 valores_de_n = [10, 100, 500, 1000]
 
 gauss_a, t_gauss_a = [], []
 ptlu_a, t_ptlu_a = [], []
 gauss_b, t_gauss_b = [], []
 ptlu_b, t_ptlu_b = [], []
+cholesky_a, t_cholesky_a = [], []
+cholesky_b, t_cholesky_b = [], []
 
 # 10 matrizes e 10 vetores
 print('\n----- Letra a) 10 matrizes e 10 vetores -----\n')
 for n in valores_de_n:
+    # ----------------------------------- ELIMINAÇÃO GAUSSIANA -----------------------------------
+
     inicio = time()
     for i in range(10):
         t1 = time()
@@ -87,6 +114,8 @@ for n in valores_de_n:
     gauss_a.append(n)
     t_gauss_a.append(fim - inicio)
 
+    # ----------------------------------- DECOMPOSIÇÃO PtLU -----------------------------------
+
     inicio = time()
     for i in range(10):
         t1 = time()
@@ -97,7 +126,7 @@ for n in valores_de_n:
 
         P, L, U = decomposicao_PtLU(A)
         Pb = np.dot(P, b)
-        y = substituicao_inversa(L, Pb)
+        y = substituicao_direta(L, Pb)
         x = substituicao_inversa(U, y)
     fim = time()
 
@@ -105,17 +134,39 @@ for n in valores_de_n:
     ptlu_a.append(n)
     t_ptlu_a.append(fim - inicio)
 
+    # ----------------------------------- CHOLESKY -----------------------------------
+
+    inicio = time()
+    for i in range(10):
+        t1 = time()
+        A = positiva_definida(n)
+        b = 100 * np.random.rand(n)
+        t2 = time()
+        inicio -= (t2 - t1) # descontando o tempo para gerar a matriz e o vetor
+
+        L, Lt = cholesky_decomposition(A)
+        y = substituicao_direta(L, b)
+        x = substituicao_inversa(Lt, y)
+    
+    fim = time()
+
+    print(f'Cholesky p/ n = {n}: {fim - inicio}s')
+    cholesky_a.append(n)
+    t_cholesky_a.append(fim - inicio)
+
 # 1 matriz e 10 vetores
 print('\n----- Letra b) 1 matriz e 10 vetores -----\n')
 for n in valores_de_n:
     A = positiva_definida(n)
+
+    # ----------------------------------- ELIMINAÇÃO GAUSSIANA -----------------------------------
 
     inicio = time()
     for i in range(10):
         t1 = time()
         b = 100 * np.random.rand(n)
         t2 = time()
-        inicio -= (t2 - t1) # descontando o tempo para gerar a matriz e o vetor
+        inicio -= (t2 - t1) # descontando o tempo para gerar o vetor
 
         x = eliminacao_gaussiana(A, b)
     fim = time()
@@ -124,19 +175,21 @@ for n in valores_de_n:
     gauss_b.append(n)
     t_gauss_b.append(fim - inicio)
 
+    # ----------------------------------- DECOMPOSIÇÃO PtLU -----------------------------------
+    
     P, L, U = None, None, None
     inicio = time()
     for i in range(10):
         t1 = time()
         b = 100 * np.random.rand(n)
         t2 = time()
-        inicio -= (t2 - t1) # descontando o tempo para gerar a matriz e o vetor
+        inicio -= (t2 - t1) # descontando o tempo para gerar o vetor
 
         if P is None:
             P, L, U = decomposicao_PtLU(A)
 
         Pb = np.dot(P, b)
-        y = substituicao_inversa(L, Pb)
+        y = substituicao_direta(L, Pb)
         x = substituicao_inversa(U, y)
     fim = time()
 
@@ -144,21 +197,42 @@ for n in valores_de_n:
     ptlu_b.append(n)
     t_ptlu_b.append(fim - inicio)
 
+    # ----------------------------------- CHOLESKY -----------------------------------
+    L, Lt = None, None
+    inicio = time()
+    for i in range(10):
+        t1 = time()
+        b = 100 * np.random.rand(n)
+        t2 = time()
+        inicio -= (t2 - t1) # descontando o tempo para gerar o vetor
+
+        if L is None:
+            L, Lt = cholesky_decomposition(A)
+        y = substituicao_direta(L, b)
+        x = substituicao_inversa(Lt, y)
+    fim = time()
+
+    print(f'Cholesky p/ n = {n}: {fim - inicio}s')
+    cholesky_b.append(n)
+    t_cholesky_b.append(fim - inicio)
+
 figure, axis = plt.subplots(2, 1)
 axis[0].plot(gauss_a, t_gauss_a, label='Eliminação Gaussiana (10 matrizes e 10 vetores)', marker='o')
 axis[0].plot(ptlu_a, t_ptlu_a, label='Decomposição PtLU (10 matrizes e 10 vetores)', marker='o')
+axis[0].plot(cholesky_a, t_cholesky_a, label='Cholesky (10 matrizes e 10 vetores)', marker='o')
 axis[0].set_title('10 matrizes e 10 vetores')
 axis[0].set_xlabel('n')
 axis[0].set_ylabel('Tempo (s)')
-axis[0].legend(['Eliminação Gaussiana', 'Decomposição PtLU'])
+axis[0].legend(['Eliminação Gaussiana', 'Decomposição PtLU', 'Cholesky'])
 axis[0].grid(True)
 
 axis[1].plot(gauss_b, t_gauss_b, label='Eliminação Gaussiana (1 matriz e 10 vetores)', marker='o')
 axis[1].plot(ptlu_b, t_ptlu_b, label='Decomposição PtLU (1 matriz e 10 vetores)', marker='o')
+axis[1].plot(cholesky_b, t_cholesky_b, label='Cholesky (1 matriz e 10 vetores)', marker='o')
 axis[1].set_title('1 matriz e 10 vetores')
 axis[1].set_xlabel('n')
 axis[1].set_ylabel('Tempo (s)')
-axis[1].legend(['Eliminação Gaussiana', 'Decomposição PtLU'])
+axis[1].legend(['Eliminação Gaussiana', 'Decomposição PtLU', 'Cholesky'])
 axis[1].grid(True)
 
 plt.show()
