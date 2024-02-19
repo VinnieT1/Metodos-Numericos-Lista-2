35aae1000e44d299ffaf74e35b8d03d4235da718
Vinícius Teixeira <92376175+VinnieT1@users.noreply.github.com>
2024-02-19 20:53:11 -0300

    correctede positive definite matrix generator

--- q3.py
+++ q3.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def positiva_definida(n):
-    A = 100 * np.random.rand(n, n)
+    A = np.random.rand(n, n)
     A = 0.5 * (A + A.T)
     A = A + n * np.eye(n)
     print(A)
--- q5.py
+++ q5.py
@@ -57,7 +57,7 @@ def decomposicao_PtLU(A):
     return P, L, U
 
 def positiva_definida(n):
-    A = 100 * np.random.rand(n, n)
+    A = np.random.rand(n, n)
     A = 0.5 * (A + A.T)
     A = A + n * np.eye(n)
     return A
